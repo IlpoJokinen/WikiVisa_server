3db4b974f1e7337ad7ba4d790095a8e39b0f4890
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-02-06 17:04:53 +0200

    dev branch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
+
 app.use(express.static('./client/build'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
