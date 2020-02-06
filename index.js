87368e09db25606238b3f2991673494ba8628b27
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-02-06 17:30:56 +0200

    implemented socket.io and created listener for click action

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
-
+const socket = require("socket.io")
 
 app.use(express.static('./client/build'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const io = socket(server)
+
+io.on("connection", (socket) => {
+    console.log("socket created")
+
+    socket.on("click", (data) => {
+        socket.broadcast.emit("someoneClicked", data)
+        socket.emit("senderConfirmation", "message sent")
+    })
+})
