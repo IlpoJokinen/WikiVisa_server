435e61fdd0a8881dfdfb728ac30527743bfde703
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-02-22 13:42:56 +0200

    Added GameCounter function that sends different timers with certain intervals

--- index.js
+++ index.js
@@ -25,19 +25,35 @@ function getQuestion(type) {
     }
 }
 
-function createGame() {
-    games.push({startGameCounter: 20, question: question, questionCounter: 20, roundEndCounter: 20, view: "startGame"})
-    setInterval(() => {
+ function createGame() {
+    games.push({
+        startGameCounter: 20, 
+        question: question, 
+        questionCounter: 20, 
+        roundEndCounter: 20, 
+        view: "startGame"
+    })
+    const GameCounter = setInterval(() => {
         games[0] = {...games[0], startGameCounter: games[0].startGameCounter - 1}
-        io.emit("send timer", games[0].startGameCounter)
+        io.emit("send startTimer", games[0].startGameCounter)
+        if (games[0].startGameCounter <= 0) {
+            games[0] = {...games[0], questionCounter: games[0].questionCounter - 1}
+            io.emit("send questionTimer", games[0].questionCounter)
+        }
+        if (games[0].questionCounter <= 0) {
+            games[0] = {...games[0], roundEndCounter: games[0].roundEndCounter - 1}
+            io.emit("send roundEndTimer", games[0].roundEndCounter)
+        } 
+        if (games[0].roundEndCounter <= -2) {
+            clearInterval(GameCounter)
+        } 
     }, 1000)
-}
+} 
 
 
 io.on("connection", (socket) => { 
     
     socket.on("get question", () => {
-        console.log("question", question)
         question.then(data => {
             io.emit("send question", {
                 question: data.title,
@@ -48,21 +64,17 @@ io.on("connection", (socket) => {
     socket.on("join game", (data) => {
         if(!games.length){
             createGame() 
-
-        }
-        console.log(data)
+        } 
         players.push({
             id: socket.id,  
             gamertag: data.gamertag,
             answers: [],
             points: 0
         })
-     
         io.emit("send players", players)
-        socket.emit("send game", games[0])
+        socket.emit("send game", games)
     })
     socket.on("get players", () => {
-        console.log("here")
         socket.emit("send players", players)
     })
     
