6ea7000d3f71c0c0a56a9c028a2acc3374f92e80
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-03-29 10:14:46 +0300

    removed mongoes

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const socket = require('socket.io')
-const mongoose = require('mongoose')
-const { MongoMemoryServer } = require('mongodb-memory-server')
-const mongod = new MongoMemoryServer()
 const port = process.env.PORT || 3001
-const User = require('./models/Test_Schema')
 const utils = require('./Utilities')
 const games = []
 let game_id = 0
@@ -19,21 +15,6 @@ const io = socket(server)
 const Game = require('./classes/Game')(io)
 
 app.use(cors())
-
-connectToMongo()
-
-async function connectToMongo() {
-    const uri = await getUri()
-    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-}
-async function getUri(){
-    const uri = await mongod.getUri()
-    return uri
-}
-app.get("/api", async (req, res) => {
-    const userFromDb = await User.find({})
-    res.json({userFromDb})
-})
 
 function createGame(roomCode, properties) {
     properties.id = game_id
