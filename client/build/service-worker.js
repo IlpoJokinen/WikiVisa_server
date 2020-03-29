be5b4c54d3f410106aa0b7412b09dbca0e3a8d41
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-03-29 14:50:21 +0300

    Build front-end sprint2 end

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcda6b53ce3a8ec90366a6290c05da8e.js"
+  "/precache-manifest.8e099c03f3621d892108b3224335d27a.js"
 );
 
 self.addEventListener('message', (event) => {
