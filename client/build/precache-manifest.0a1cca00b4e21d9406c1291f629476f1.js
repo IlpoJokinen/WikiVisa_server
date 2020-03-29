e7d0174f082bac0ac2c5d51cd1b91a450a9413c8
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-03-30 01:47:49 +0300

    added new build

---
+++ client/build/precache-manifest.0a1cca00b4e21d9406c1291f629476f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fb183a7af8662337c3b3d0e9c7ef4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a030af74f243db41fd91",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6336eaf180980b0b8ad4",
+    "url": "/static/css/main.d57024f2.chunk.css"
+  },
+  {
+    "revision": "a030af74f243db41fd91",
+    "url": "/static/js/2.25264d2a.chunk.js"
+  },
+  {
+    "revision": "b5c235dc1af9a60024e1b626da23d986",
+    "url": "/static/js/2.25264d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6336eaf180980b0b8ad4",
+    "url": "/static/js/main.b6efce6a.chunk.js"
+  },
+  {
+    "revision": "118b2065da512dc6e575",
+    "url": "/static/js/runtime-main.b887d2a1.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e099c03f3621d892108b3224335d27a.js"
+  "/precache-manifest.0a1cca00b4e21d9406c1291f629476f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
