65e654992434ac073ab83633754c871bfa23b98a
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-04-05 14:12:40 +0300

    New build

---
+++ client/build/precache-manifest.412c1192b8c6b35acaf6ec404fd1688f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9cdbf4333561f70cfecc492f92b9d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e09d50ecc36e04d56a3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9837ae5f1b70d132b543",
+    "url": "/static/css/main.487792cd.chunk.css"
+  },
+  {
+    "revision": "1e09d50ecc36e04d56a3",
+    "url": "/static/js/2.6e26e914.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.6e26e914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9837ae5f1b70d132b543",
+    "url": "/static/js/main.8cef02d2.chunk.js"
+  },
+  {
+    "revision": "c7ac01c548099df1e5e3",
+    "url": "/static/js/runtime-main.305cf52f.js"
+  }
+]);
