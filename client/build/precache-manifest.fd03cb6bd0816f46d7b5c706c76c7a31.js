cad244ae28198818cd31c60114ca922997a5c57d
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-04-05 15:23:43 +0300

    new build with fixed joingame function

---
+++ client/build/precache-manifest.fd03cb6bd0816f46d7b5c706c76c7a31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8503f8a1b8b87ff6bcafdd0b463a7857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e09d50ecc36e04d56a3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1fd0a89825ad6cfdd062",
+    "url": "/static/css/main.487792cd.chunk.css"
+  },
+  {
+    "revision": "1e09d50ecc36e04d56a3",
+    "url": "/static/js/2.6e26e914.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.6e26e914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd0a89825ad6cfdd062",
+    "url": "/static/js/main.a5a7cd18.chunk.js"
+  },
+  {
+    "revision": "c7ac01c548099df1e5e3",
+    "url": "/static/js/runtime-main.305cf52f.js"
+  }
+]);
